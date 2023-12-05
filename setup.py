62cfc0efc4ffdfcea604af4f00bb7a09c4cb2059
hakimkse <5586611+hakimkhalafi@users.noreply.github.com>
2023-12-05 18:31:13 +0100

    Add project website

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-# Read the contents of your README file
+# Read the contents of README file
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
