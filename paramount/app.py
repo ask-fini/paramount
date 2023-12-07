3242bf214f52c79e42705f44d77436339ba9c653
hakimkse <5586611+hakimkhalafi@users.noreply.github.com>
2023-12-07 20:32:18 +0100

    DF filtering

--- paramount/app.py
+++ paramount/app.py
@@ -5,19 +5,38 @@ from glob import glob
 
 def run():
     st.title('Paramount')
-
-    # Loop through all files that match the pattern 'paramount_data_*.csv'
     files = sorted(glob('paramount_data_*.csv'))
+
+    df_list = []
+
+    possible_cols = []
+
     if len(files) > 0:
-        for file in files:
-            # Read the CSV file into a DataFrame
-            df = pd.read_csv(file)
-            # Use st.data_editor to display the DataFrame
-            st.data_editor(data=df, use_container_width=True)
-            # Add some space after each table for readability
-            st.write("---")
+        for idx, file in enumerate(files):
+            temp_df = pd.read_csv(file)
+            df_list.append(temp_df)
+            possible_cols += [col for col in temp_df.columns if col not in possible_cols]
     else:
         st.write("No data files found. Ensure you use @paramount.record decorator on any functions you want to record.")
+
+    input_cols = [col for col in possible_cols if not col.startswith("output_") or not col.split("_")[1].isdigit()]
+    output_cols = [col for col in possible_cols if col.startswith("output_") and col.split("_")[1].isdigit()]
+    identifier_cols = input_cols
+
+    selected_id_cols = st.multiselect('Identifiers', identifier_cols)
+    selected_input_cols = st.multiselect('Input columns', input_cols)
+    selected_output_cols = st.multiselect('Output columns', output_cols)
+
+    if not selected_id_cols and not selected_input_cols and not selected_output_cols:
+        filtered_cols = possible_cols
+    else:
+        filtered_cols = selected_id_cols + selected_input_cols + selected_output_cols
+
+    df_list = [df[filtered_cols] for df in df_list]
+
+    for idx, df in enumerate(df_list):
+        st.data_editor(data=df, key=f"df_editor_{idx}", use_container_width=True)
+        st.write("---")
 
 
 if __name__ == '__main__':
