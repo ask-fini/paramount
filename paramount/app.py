77d3df8a9aeb8984f6fd9e678f53fc77af20e8b2
hakimkse <5586611+hakimkhalafi@users.noreply.github.com>
2023-12-08 15:12:44 +0100

    Merged DF, timestamps, ground truth

--- paramount/app.py
+++ paramount/app.py
@@ -16,27 +16,26 @@ def run():
             temp_df = pd.read_csv(file)
             df_list.append(temp_df)
             possible_cols += [col for col in temp_df.columns if col not in possible_cols]
+
+        input_cols = [col for col in possible_cols if not col.startswith("output_") or not col.split("_")[1].isdigit()]
+        output_cols = [col for col in possible_cols if col.startswith("output_") and col.split("_")[1].isdigit()]
+        identifier_cols = input_cols
+
+        paramount_cols = ['paramount_timestamp', 'paramount_ground_truth']
+        selected_id_cols = st.multiselect('Identifiers', identifier_cols)
+        selected_input_cols = st.multiselect('Input columns', input_cols)
+        selected_output_cols = st.multiselect('Output columns', output_cols)
+
+        if not selected_id_cols and not selected_input_cols and not selected_output_cols:
+            filtered_cols = possible_cols
+        else:
+            filtered_cols = paramount_cols + selected_id_cols + selected_input_cols + selected_output_cols
+
+        df_merged = pd.concat([df[filtered_cols] for df in df_list])
+
+        st.data_editor(data=df_merged, use_container_width=True)
     else:
         st.write("No data files found. Ensure you use @paramount.record decorator on any functions you want to record.")
-
-    input_cols = [col for col in possible_cols if not col.startswith("output_") or not col.split("_")[1].isdigit()]
-    output_cols = [col for col in possible_cols if col.startswith("output_") and col.split("_")[1].isdigit()]
-    identifier_cols = input_cols
-
-    selected_id_cols = st.multiselect('Identifiers', identifier_cols)
-    selected_input_cols = st.multiselect('Input columns', input_cols)
-    selected_output_cols = st.multiselect('Output columns', output_cols)
-
-    if not selected_id_cols and not selected_input_cols and not selected_output_cols:
-        filtered_cols = possible_cols
-    else:
-        filtered_cols = selected_id_cols + selected_input_cols + selected_output_cols
-
-    df_list = [df[filtered_cols] for df in df_list]
-
-    for idx, df in enumerate(df_list):
-        st.data_editor(data=df, key=f"df_editor_{idx}", use_container_width=True)
-        st.write("---")
 
 
 if __name__ == '__main__':
--- paramount/record.py
+++ paramount/record.py
@@ -3,6 +3,7 @@ import json
 import inspect
 import os
 from datetime import datetime
+import pytz
 
 
 def is_jsonable(x):
@@ -46,8 +47,14 @@ def record(func):
         result = func(*args, **kwargs)
         serialized_result = serialize_response(result)
 
-        # Create a DataFrame with a single row of the args_dict and result
-        result_data = {**args_dict}
+        # Get current UTC timestamp
+        timestamp_now = datetime.now(pytz.timezone('UTC')).replace(microsecond=0).isoformat()
+
+        # Update result data dictionary with timestamp
+        result_data = {
+            'paramount_timestamp': timestamp_now,
+            'paramount_ground_truth': False,
+            **args_dict}
         for i, output in enumerate(serialized_result, start=1):
             if isinstance(output, dict):
                 for key, value in output.items():
@@ -55,17 +62,17 @@ def record(func):
             else:
                 result_data[f'output_{i}'] = output
         df = pd.DataFrame([result_data])
+        df['paramount_timestamp'] = pd.to_datetime(df['paramount_timestamp'])
+        df['paramount_ground_truth'] = df['paramount_ground_truth'].astype('bool')
 
         # Determine the filename based on the current date
-        filename = f"paramount_data_{datetime.now().strftime('%Y_%m_%d')}.csv"
+        filename = f"paramount_data_{datetime.utcnow().strftime('%Y_%m_%d')}.csv"
 
         # Check if the file exists, and if not, create it with header, else append without header
         if not os.path.isfile(filename):
             df.to_csv(filename, mode='a', index=False)
         else:
             df.to_csv(filename, mode='a', index=False, header=False)
-
-        print(f"Args and kwargs: {json.dumps(args_dict, indent=2)}, Result: {json.dumps(serialized_result, indent=2)}")
 
         return result
 
