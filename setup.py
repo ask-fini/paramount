ea02d2678baaa5590eb6368bce59263b45c35fa7
hakimkse <5586611+hakimkhalafi@users.noreply.github.com>
2024-03-28 17:20:42 +0100

    setup py version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
    name='paramount',
-   version='0.2.5',
+   version='0.3.0',
    description='Business Evals for LLMs',
    long_description=long_description,
    long_description_content_type='text/markdown',
